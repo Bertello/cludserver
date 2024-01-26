65a88cbbd5bb181d87e9060fbaf0544249a07c07
Bertello <92369564+Bertello@users.noreply.github.com>
2024-01-26 09:08:42 +0100

    modifica cors options
    modifica cors options

--- server.ts
+++ server.ts
@@ -67,14 +67,38 @@ app.use("/", (req:any, res:any, next:any) => {
     next();
 });
 
-//5 CORS (Controllo degli accessi)
+const whitelist = [
+    "http://cludserverbertello.onrender.com", // porta 80 (default)
+    "https://cludserverbertello.onrender.com", // porta 443 (default)
+    "http://localhost:3000",
+    "https://localhost:3001",
+    "http://localhost:4200" // server angular
+];
+
 const corsOptions = {
+    origin: function(origin, callback) {
+    if (!origin) // browser direct call
+    return callback(null, true);
+    if (whitelist.indexOf(origin) === -1) {
+    var msg = `The CORS policy for this site does not
+    allow access from the specified Origin.`
+    return callback(new Error(msg), false);
+    }
+    else
+    return callback(null, true);
+    },
+    credentials: true
+   };
+   app.use("/", _cors(corsOptions));
+
+//5 CORS (Controllo degli accessi)
+/*const corsOptions = {
     origin: function(origin, callback) {
         return callback(null, true);
     },
     credentials: true
 };
-app.use("/", _cors(corsOptions));
+app.use("/", _cors(corsOptions));*/
 
 //********************************************************************************/
 // Routes utente
